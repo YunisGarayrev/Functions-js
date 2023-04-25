c56f9d98fead713e08637257804d48d74969f5a4
YunisGarayev <79d2mw0@code.edu.az>
2023-04-25 09:48:58 -0700

    functions js

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+
+// a. Toplama çıxma, toplama, vurma, bölməyə uyğun funksiya yaradirsiz 2 eded ve 1 operator('+','-','*','/') qebul edir.
+//  Qebul etdiyi operatora uygun a və b ədədləri üzərində hesablama aparib alinan deyeri qaytarır.
+
+// function numberOperations(a,b) {
+//     var c;
+//     var operation = prompt("Istifadə edəcəyiniz əməliyyatin adini daxil edin");
+//     switch (operation) {
+//         case "toplama":
+//             c = a + b;
+//             break;
+//         case "cixma":
+//             c = a - b;
+//             break;
+//         case "vurma":
+//             c = a * b;
+//             break;
+//         case "bolme":
+//             c = a / b;
+//             break;
+
+
+//     }
+//     return c;
+
+// }
+// console.log(numberOperations(prompt("ilkin ededi daxil edin:"),prompt("ikinci ededi daxil edin:")))
+
+
+
+
+
+// b.Methodlar yaradirsiz. Methoda ad gonderende adi ekrana cixardir(Sabir),
+//  ad ve soyad gonderende soyadi adi ekrana cixardir(Guliyev Sabir), ad,soyad ve 
+//  ata adı göndərildikdə adin baş hərfi nöqtə soyad ata adi ekrana cixardirsiz(S.Guliyev Mehti). 
+//  Methodlarin adi eyni olmalıdır.
+
+ 
+// firstName= "Yunis";
+// lastName= "Garayev";
+// fathername= "Ali";
+// myname = function() {
+// return firstName ;
+// };
+
+// surname = function() {
+// return lastName + " " + firstName;
+// };
+// fullname = function() {
+// return firstName[0] +"." + lastName + " " + fathername;
+// };
+
+
+// console.log(fullname());
+
+
+
+
+
+  // c. 1 eded daxil edin. Eger o eded 50den kicikdirse o edede qeder 3-e bolunen, 
+  // 50-100 araligindadirsa o edede qeder 5e bolunen, 
+  // 100den boyukdurse o edede qeder 8 e bolunen ededlerin sayini ekrana cixardan bir alqoritm yazin.
+
+  // function result(N) {
+  //   var say=0;
+  //   for (var num =1; num < N; num++) {
+  //     if (N < 50 && num % 3 == 0)
+  //       say++;
+      
+  //     else if (N>50 && N<100 && num % 5 == 0)
+  //       say++;
+  
+  //     else if(N>=100 && num % 8 == 0)
+  //       say++;
+  //   }
+  //   return say;
+  // }
+  // console.log(result(prompt()));
+
+
+
+
+   
+
+
+
+
+
+   
+
